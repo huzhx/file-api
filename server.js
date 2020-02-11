94f6947efce5c63029065c6fa06c72fc64fec578
Zhaoxian Hu <huzhx@uci.edu>
2020-02-11 15:50:13 -0800

    implement the get /files route in the server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const { allowCORS } = require('./middlewares/allowCORS');
 require('./config/config');
+const files = require('./routes/files');
 
 const app = express();
 const port = process.env.PORT;
@@ -11,6 +12,8 @@ const port = process.env.PORT;
 app.use(allowCORS);
 
 app.use(bodyParser.json());
+
+app.use('/files', files);
 
 if (!module.parent) {
   app.listen(port, () => console.log('started on port ', port));
